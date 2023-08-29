d39041e5e27a9c51519e577cc59fc8c811b8f2aa
Deepu Kumar <kumardeepu1066@gmail.com>
2023-08-29 17:56:07 +0530

    new file:   .gitignore

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-logo-concept-by-kim-barsegyan-dribbble.jpg";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
